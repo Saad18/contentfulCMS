f76ff892d88d55f4bcc1fc3507094aa4cafad86b
nurul-haque-saad <atsaadhaque@gmail.com>
2023-12-13 00:08:14 +0600

    complete with data fatching

--- src/fetchProjects.jsx
+++ src/fetchProjects.jsx
@@ -1,13 +1,38 @@
+import { useState, useEffect } from 'react';
 import { createClient } from "contentful";
 
 const client = createClient({
 
-    space:  'vbikt4bhx25h',
-    environment: 'master',
+    space: 'vbikt4bhx25h',
+    environment: 'master', // master is default
     accessToken: 'dC-1lecCkA4MKUOS7SKgKhaXgBLv8QgpOnY4WoXhfug',
 
 });
 
-client
-.getEntries({content_type: 'projects'})
-.then((Response)=> console.log(Response));
+export const useFetchProjects = () => {
+    const [loading, setLoading] = useState(true);
+    const [projects, setProjects] = useState([]);
+
+    const getData = async () => {
+        try {
+            const response = await client.getEntries({ content_type: 'projects' });
+            const projects = response.items.map((item) => {
+                const { title, url, image } = item.fields //destructuring
+                const id = item.sys.id
+                const img = image?.fields?.file?.url // check the nested location
+                return { title, url, id, img }
+            });
+            setProjects(projects)
+            setLoading(false);
+        } catch (error) {
+            console.log(error);
+            setLoading(false);
+        }
+    }
+
+    useEffect(() => {
+        getData();
+    }, []);
+
+    return { loading, projects };
+}
